838e8232652956a217e0dc034e8fc9ef949ddab3
keithhans <keithhans@gmail.com>
2024-11-24 22:09:06 +0800

    add tensorboard

--- src/playground.py
+++ src/playground.py
@@ -1,9 +1,19 @@
 from options import Options
 from painter import Painter
+import datetime
+from my_tensorboard import TensorBoard
+
+
 
 opt = Options()
 opt.gather_options()
-opt.writer=None
+
+# Setup Tensorboard
+date_and_time = datetime.datetime.now()
+run_name = '' + date_and_time.strftime("%m_%d__%H_%M_%S")
+opt.writer = TensorBoard('{}/{}'.format('tensorboard', run_name))
+#opt.writer.add_text('args', str(sys.argv), 0)
+
 opt.robot=None
 
 painter = Painter(opt) 
